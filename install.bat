44dc24cbd52f23ab66f8532b3f02642b34f0d5b5
Prayag <prayagprajapati.17@gmail.com>
2021-05-16 11:24:46 +0530

    Add subop

--- install.bat
+++ install.bat
@@ -2,9 +2,6 @@
 @echo This will not work if you have installed jellyfin to a custom location
 set /p ask = Enter 'X' to continue or 'C' to cancel: 
 if "%ask%" neq "x" goto:eof
-
-    @echo starting install...
-
-    dotnet publish --configuration Release --output bin
-
-    move bin\Jellyfin.Plugin.SkinManager.dll C:\ProgramData\Jellyfin\Server\plugins\SkinManager
+@echo starting install...
+dotnet publish --configuration Release --output bin
+move bin\Jellyfin.Plugin.SkinManager.dll C:\ProgramData\Jellyfin\Server\plugins\SkinManager
